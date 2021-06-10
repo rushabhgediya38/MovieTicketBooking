a1e7392e59f3e7b881cc9fb9c9118d284d41e992
rushabh <rushabhgediya38@gmail.com>
2021-06-10 13:15:01 +0530

    initial commit

---
+++ MovieUser/views.py
@@ -0,0 +1,101 @@
+from django.contrib import messages
+from django.shortcuts import render
+from django.contrib.auth.models import User
+
+# Create your views here.
+
+
+def login(request):
+    return render(request, 'sign-in.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        username1 = request.POST['username']
+        firstname1 = request.POST['firstname']
+        lastname1 = request.POST['lastname']
+        email1 = request.POST['email']
+        email = email1.lower()
+        password = request.POST['password1']
+        password1 = request.POST['password2']
+
+        context = {
+            'has_error': False
+        }
+
+        if firstname1 == '':
+            messages.add_message(request, messages.ERROR,
+                                 'Firstname is required')
+            context['has_error']: True
+
+        if lastname1 == '':
+            messages.add_message(request, messages.ERROR,
+                                 'Lastname is required')
+            context['has_error']: True
+
+        if password == '':
+            messages.add_message(request, messages.ERROR,
+                                 'Password is required')
+            context['has_error']: True
+
+        if password1 == '':
+            messages.add_message(request, messages.ERROR,
+                                 'Confirm Password is required')
+            context['has_error']: True
+
+        # password should match
+        if password != password1:
+            messages.add_message(request, messages.ERROR, 'Password Not Match')
+            context['has_error'] = True
+
+        if len(password) < 8:
+            messages.add_message(request, messages.ERROR,
+                                 'password shod be Atleast 8 character or more')
+            context['has_error'] = True
+
+        if context['has_error']:
+            return render(request, 'sign-up.html', context, status=400)
+
+        ''' Begin reCAPTCHA validation Only Work On Real Website'''
+        # recaptcha_response = request.POST.get('g-recaptcha-response')
+        # url = 'https://www.google.com/recaptcha/api/siteverify'
+        # values = {
+        #     'secret': settings.GOOGLE_RECAPTCHA_SECRET_KEY,
+        #     'response': recaptcha_response
+        # }
+        # data = urllib.parse.urlencode(values).encode()
+        # req = urllib.request.Request(url, data=data)
+        # response = urllib.request.urlopen(req)
+        # result = json.loads(response.read().decode())
+
+        # if result['success']:
+        #     user = User.objects.create_user(
+        #         username=username1, email=email, password=password1)
+        #     user.first_name = firstname1
+        #     user.last_name = lastname1
+        #     user.is_active = True
+        #     user.save()
+        #     messages.add_message(request, messages.SUCCESS,
+        #                          'Account Successfully Created.')
+        #     return render(request, 'userall/login.html')
+
+        # else:
+        #     messages.error(request, 'Invalid reCAPTCHA. Please try again.')
+        #     return render(request, 'userall/register.html')
+
+        ''' End reCAPTCHA validation '''
+
+        user = User.objects.create_user(
+            username=username1, email=email, password=password1)
+
+        user.first_name = firstname1
+        user.last_name = lastname1
+        user.is_active = True
+        user.save()
+
+        messages.add_message(request, messages.SUCCESS,
+                             'Account Successfully Created.')
+        return render(request, 'sign-in.html')
+
+    else:
+        return render(request, 'sign-up.html')
